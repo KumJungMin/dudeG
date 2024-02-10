d02ee5678b22fdfab0318a7d0519e3f7f79aef83
KumJungMin <rmawjdals@naver.com>
2024-02-10 17:06:28 +0900

    feat: 유저 이미지, 닉네임 섹션 추가하기

--- src/components/room/UserProfile.tsx
+++ src/components/room/UserProfile.tsx
@@ -1,14 +1,23 @@
 'use client';
 
-import { useSession } from 'next-auth/react';
 import React from 'react';
+import { useSession } from 'next-auth/react';
+import Image from 'next/image';
+
+import Style from './UserProfile.module.scss';
 
 export default function UserProfile() {
   const { data: session } = useSession();
   return (
-    <div>
-      {session?.user?.email} {session?.user?.name}
-      {JSON.stringify(session)}
+    <div className={Style.container}>
+      <Image
+        src={session?.user?.image || '/'}
+        width={40}
+        height={40}
+        style={{ objectFit: 'cover', borderRadius: '50%' }}
+        alt="user profile"
+      />
+      <h3 className={Style.name}>{session?.user?.name}님의 대기실</h3>
     </div>
   );
 }
