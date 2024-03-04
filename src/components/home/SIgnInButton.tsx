046926febe107903eb212b6467ff20ad392625c1
KumJungMin <rmawjdals@naver.com>
2024-03-04 22:34:34 +0900

    chore: 테스트 함수 제거하기

--- src/components/home/SIgnInButton.tsx
+++ src/components/home/SIgnInButton.tsx
@@ -1,13 +1,7 @@
 'use client';
-// import { signIn } from 'next-auth/react'; TODO: next-auth/react를 사용하여 사용자 인증하기
-import { getTestData } from '@/src/lib/fetchers';
+import { signIn } from 'next-auth/react'; // TODO: next-auth/react를 사용하여 사용자 인증하기
 import Button from '../button/Button';
 
 export default function SignInButton(): JSX.Element {
-  async function testCall() {
-    // TODO: 몽고 DB 연결하면서 signIn() 함수를 사용하여 사용자 인증하기
-    const response = await getTestData();
-    console.log(response);
-  }
-  return <Button click={() => testCall()}>꾹 눌러 시작하기</Button>;
+  return <Button click={() => signIn()}>꾹 눌러 시작하기</Button>;
 }
