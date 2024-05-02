3f2a229d2e05d22fb4184061b3b81bf4b1b0b471
KumJungMin <rmawjdals@naver.com>
2024-05-02 21:56:04 +0900

    feat: 스토어 정보 불러오기

--- src/components/room/GuestList.tsx
+++ src/components/room/GuestList.tsx
@@ -1,19 +1,18 @@
 import React from 'react';
+import { useUserStore } from '@/src/stores/useUserStore';
+
 import GuestListItem from './GuestListItem';
 import KakaoShareButton from './KakaoShareButton';
 
 import Style from './GuestList.module.scss';
 
 export default function GuestList() {
-  const GUEST_LIST = [
-    { id: 1, name: '김철수1', image: '/' },
-    { id: 2, name: '김철수2', image: '/' },
-  ];
+  const { guests } = useUserStore();
 
   return (
     <div className={Style.guestList}>
       <div className={Style.container}>
-        {GUEST_LIST.map((guest) => (
+        {Object.values(guests).map((guest) => (
           <GuestListItem key={guest.id} name={guest.name} image={guest.image} />
         ))}
       </div>
--- src/components/room/UserProfile.tsx
+++ src/components/room/UserProfile.tsx
@@ -1,24 +1,25 @@
 'use client';
 
 import React from 'react';
-import { useSession } from 'next-auth/react';
 import Image from 'next/image';
+import { useUserStore } from '@/src/stores/useUserStore';
 
 import Style from './UserProfile.module.scss';
 
 export default function UserProfile() {
-  const { data: session } = useSession();
+  const { user } = useUserStore();
+
   return (
     <div className={Style.container}>
       <Image
-        src={session?.user?.image || '/'}
+        src={user.image || '/'}
         width={40}
         height={40}
         style={{ objectFit: 'cover', borderRadius: '50%' }}
         alt="user profile"
       />
       <h3 className={Style.nameWrapper}>
-        <span className={Style.name}>{session?.user?.name || '사용자'}</span>
+        <span className={Style.name}>{user.name || '사용자'}</span>
         <span className={Style.text}>의 대기실</span>
       </h3>
     </div>
