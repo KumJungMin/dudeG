42b223863d2f07d35a085a54796bc250cf31b11d
KumJungMin <rmawjdals@naver.com>
2024-05-03 21:07:56 +0900

    refactor: scss 변수를 global로 지정하기

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
+  sassOptions: {
+    includePaths: ['./src'],
+    prependData: `@import "styles/variables.scss";`,
+  },
   images: {
     remotePatterns: [
       {
